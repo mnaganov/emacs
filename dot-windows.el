684eb9e15eb4c8bc814c46bbde363bbeab590e05
Mikhail Naganov <mnaganov@MNAGANOV-QUADXP.ad.corp.google.com>
2009-05-26 12:27:03 +0400

    updated windows configuration

--- dot-windows.el
+++ dot-windows.el
@@ -1,18 +1,3 @@
 ;; Windows-specific part
 
-;; Recognize Cygwin's path names
-(setq exec-path (cons "c:/cygwin/bin/" exec-path))
-(require 'cygwin-mount)
-(cygwin-mount-activate)
-
-;; Replace DOS shell with Cygwin Bash
-(add-hook 'comint-output-filter-functions
-    'shell-strip-ctrl-m nil t)
-(add-hook 'comint-output-filter-functions
-    'comint-watch-for-password-prompt nil t)
-(setq explicit-shell-file-name "bash.exe")
-;; For subprocesses invoked via the shell
-;; (e.g., "shell -c command")
-(setq shell-file-name explicit-shell-file-name)
-(add-hook 'shell-mode-hook 
-    'ansi-color-for-comint-mode-on)
+(require 'setup-cygwin)
