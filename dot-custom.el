3556902156a034a49538070ba51d48b3fe401c86
Mikhail Naganov <mnaganov@dhcp-172-28-174-226.spb.corp.google.com>
2009-08-05 19:05:50 +0400

    use control as meta in cocoa emacs

--- dot-custom.el
+++ dot-custom.el
@@ -4,9 +4,11 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(js2-auto-indent-p nil)
+ '(js2-cleanup-whitespace nil)
  '(js2-indent-on-enter-key nil)
  '(js2-mirror-mode nil)
- '(js2-cleanup-whitespace nil))
+ '(ns-command-modifier (quote meta))
+ '(ns-use-qd-smoothing nil))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
