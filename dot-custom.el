65bc48c02fa8f3b0f5d66a9b45de6f1708d553e1
Mikhail Naganov <mnaganov@dhcp-172-28-174-226.spb.corp.google.com>
2009-08-14 16:40:18 +0400

    customized diff mode

--- dot-custom.el
+++ dot-custom.el
@@ -13,4 +13,5 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- )
+ '(diff-added ((t (:inherit diff-changed :foreground "dark green"))))
+ '(diff-removed ((t (:inherit diff-changed :foreground "red")))))
