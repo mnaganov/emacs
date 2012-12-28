fa299b360b44f2da3b32d0cce2c7079f008ddace
Mikhail Naganov <mnaganov@chromium.org>
2012-12-28 14:10:43 +0000

    Removed usage of make-local-hook

--- dot-packages.el
+++ dot-packages.el
@@ -90,10 +90,7 @@
 ;; JS2
 (autoload 'js2-mode (format "js2-emacs%d" emacs-major-version) nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(add-hook 'js2-mode-hook
-          (lambda ()
-            (make-local-hook 'after-revert-hook)
-            (add-hook 'after-revert-hook 'js2-mode-reset nil t)))
+(add-hook 'js2-mode-hook 'js2-mode-reset nil t)
 (if use-google-stuff
     (require 'js2-google))
 
