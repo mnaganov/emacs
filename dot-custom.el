b21ef8a7a2c7ba3fb450b14afcb9431e290fff84
Mikhail Naganov <mikhail.naganov@gmail.com>
2013-05-23 16:46:04 +0100

    Updated diff colors

--- dot-custom.el
+++ dot-custom.el
@@ -14,5 +14,5 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(diff-added ((t (:inherit diff-changed :foreground "dark green"))))
+ '(diff-added ((t (:inherit diff-changed :foreground "blue"))))
  '(diff-removed ((t (:inherit diff-changed :foreground "red")))))
