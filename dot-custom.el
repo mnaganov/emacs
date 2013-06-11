5bd0cf193f089f59998db0d0a0126fc9637e248b
Mikhail Naganov <mikhail.naganov@gmail.com>
2013-06-11 10:26:45 +0100

    Custom colors for diff

--- dot-custom.el
+++ dot-custom.el
@@ -9,3 +9,10 @@
  '(js2-indent-on-enter-key nil)
  '(js2-mirror-mode nil)
  '(ns-command-modifier (quote meta)))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(diff-added ((t (:inherit diff-changed :foreground "red"))))
+ '(diff-removed ((t (:inherit diff-changed :foreground "magenta")))))
