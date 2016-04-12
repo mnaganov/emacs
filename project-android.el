2b7e6a6398fe2a4c9b7f42319c866a83ae2f502d
Mikhail Naganov <mnaganov@chromium.org>
2016-04-12 09:10:04 -0700

    Added Android project

---
+++ project-android.el
@@ -0,0 +1,51 @@
+;; Project Android stuff
+
+;; Keybindings
+
+(require 'find-things-fast)
+(require 'ack)
+(defvar my-cs-command '("cs --corpus=android   | sed -n -e 's/^android\\///p'" .  21)
+  "The command to be run by the cs function.")
+(define-compilation-mode my-cs-mode "CS"
+  "CodeSearch compilation mode."
+  nil)
+(defun my-cs (command-args)
+  (interactive
+   (list (read-shell-command "Run cs (like this): "
+                             my-cs-command
+                             'my-cs-history)))
+  (compilation-start command-args 'my-cs-mode))
+
+(global-set-key [f1] 'ftf-find-file)
+(global-set-key [f2] 'my-cs)
+(global-set-key [f5] '(lambda () (interactive) (revert-buffer nil t)))
+(global-set-key [f6] 'ack)
+(global-set-key [f7] 'compile)
+(global-set-key [f8] 'recompile)
+
+(defun set-android-coding-style ()
+   (setq c-basic-offset 4)
+   (setq fill-column 100)
+   (setq c-continued-statement-offset 8)
+   (c-set-offset 'statement-cont '++)
+   (c-set-offset 'arglist-intro '++)
+   (c-set-offset 'arglist-cont-nonempty '++))
+
+(add-hook 'c-mode-common-hook
+          (lambda()
+            (setq tags-revert-without-query t)
+            (google-set-c-style)
+            (set-android-coding-style)))
+
+(add-hook 'java-mode-hook 'set-android-coding-style)
+
+(add-hook 'python-mode-hook
+          (lambda()
+            (setq python-indent 2)))
+
+(defun open-shell-buffer (buffer-name startup-command)
+  (switch-to-buffer (shell buffer-name))
+  (set-marker comint-last-output-start (point))
+  (insert startup-command)
+  (comint-send-input nil t))
+(open-shell-buffer "=android=" "cd ~/code")
