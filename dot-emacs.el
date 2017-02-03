17e0d583ada22347c18186aa6be50492c6b27db3
Mikhail Naganov <mnaganov@chromium.org>
2017-02-02 17:17:50 -0800

    shell-procfs-dirtrack-mode

--- dot-emacs.el
+++ dot-emacs.el
@@ -199,6 +199,35 @@
 ;; Disable Undo for shell buffers
 (add-hook 'shell-mode-hook 'buffer-disable-undo)
 
+;; Track current dir in shell via procfs
+
+(defun shell-procfs-dirtrack (str)
+  (prog1 str
+         (when (string-match comint-prompt-regexp str)
+               (let ((directory (file-symlink-p
+                                 (format "/proc/%s/cwd"
+                                         (process-id
+                                          (get-buffer-process
+                                           (current-buffer)))))))
+                 (when (file-directory-p directory)
+                       (cd directory))))))
+
+(define-minor-mode shell-procfs-dirtrack-mode
+  "Track shell directory by inspecting procfs."
+  nil nil nil
+  (cond (shell-procfs-dirtrack-mode
+         (when (bound-and-true-p shell-dirtrack-mode)
+               (shell-dirtrack-mode 0))
+         (when (bound-and-true-p dirtrack-mode)
+               (dirtrack-mode 0))
+         (add-hook 'comint-preoutput-filter-functions
+                   'shell-procfs-dirtrack nil t))
+        (t
+         (remove-hook 'comint-preoutput-filter-functions
+                      'shell-procfs-dirtrack t))))
+
+(add-hook 'shell-mode-hook 'shell-procfs-dirtrack-mode)
+
 ;; Use dired as a dedicated "project tree window"
 (defun dired-buffer-live-p (buffer)
   (and (buffer-live-p buffer)
