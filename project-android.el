35d22f2864456c6792052954bed2ffafda9e7daa
Mikhail Naganov <mnaganov@google.com>
2018-01-19 09:47:27 -0800

    Added taimen buffer

--- project-android.el
+++ project-android.el
@@ -8,6 +8,8 @@
         ". build/envsetup.sh && lunch sailfish-eng && USE_GOMA=true mmm -j100 frameworks/av"
         ". build/envsetup.sh && lunch walleye-eng && USE_GOMA=true chrt -b 0 make -j100 -l30"
         ". build/envsetup.sh && lunch walleye-eng && USE_GOMA=true mmm -j100 frameworks/av"
+        ". build/envsetup.sh && lunch taimen-eng && USE_GOMA=true chrt -b 0 make -j100 -l30"
+        ". build/envsetup.sh && lunch taimen-eng && USE_GOMA=true mmm -j100 frameworks/av"
         "ANDROID_HOME=~/Android/Sdk ANDROID_NDK_HOME=~/android-ndk-r15c ./gradlew assembleDebug"))
 (setq compile-command
    ". build/envsetup.sh && lunch sailfish-eng && USE_GOMA=true chrt -b 0 make -j100 -l30")
@@ -86,6 +88,7 @@
 (open-shell-buffer "=sailfish-b=" "cd ~/code/master && . build/envsetup.sh && lunch sailfish-eng && export ANDROID_SERIAL=HT6540300151")
 (open-shell-buffer "=sailfish-w=" "cd ~/code/master && . build/envsetup.sh && lunch sailfish-eng && export ANDROID_SERIAL=FA6CP0301906")
 (open-shell-buffer "=walleye=" "cd ~/code/master && . build/envsetup.sh && lunch walleye-eng && export ANDROID_SERIAL=HT76N1A00839")
+(open-shell-buffer "=taimen=" "cd ~/code/master && . build/envsetup.sh && lunch taimen-eng && export ANDROID_SERIAL=709KPAE0017104")
 (open-shell-buffer "=toolbox=" (concat "cd ~/code/master && "
                                        ". build/envsetup.sh && lunch sailfish-eng && "
                                        "export ANDROID_SERIAL=FA6CP0301906 ANDROID_HOME=~/Android/Sdk ANDROID_NDK_HOME=~/android-ndk-r15c"))
