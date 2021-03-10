f1ea9844d255984238d155b3af899ba7360cd153
Mikhail Naganov <mnaganov@google.com>
2021-03-10 09:49:26 -0800

    Remove which-function enabling

--- dot-packages.el
+++ dot-packages.el
@@ -181,17 +181,6 @@
                "\\|Password for"))
         (add-hook 'comint-output-filter-functions 'comint-watch-for-password-prompt))
 
-;; which function
-(which-function-mode 1)
-(setq mode-line-misc-info
-      (delete (assoc 'which-function-mode mode-line-misc-info) mode-line-misc-info)
-      which-func-header-line-format '(which-func-mode ("" which-func-format)))
-(defadvice which-func-ff-hook (after header-line activate)
-  (when which-func-mode
-    (setq mode-line-misc-info
-          (delete (assoc 'which-function-mode mode-line-misc-info) mode-line-misc-info)
-          header-line-format which-func-header-line-format)))
-
 ;; Markdown
 (autoload 'markdown-mode "markdown-mode"
    "Major mode for editing Markdown files" t)
