6a1403a244997b9619327e91dd97d90e93cef1e0
Mikhail Naganov <mikhail.naganov@gmail.com>
2022-08-28 21:20:43 -0700

    Support pandoc on Windows WSL

--- project-home.el
+++ project-home.el
@@ -7,10 +7,12 @@
 (require 'find-things-fast)
 (global-set-key '[f1] 'ftf-find-file)
 
-;; On Mac, add commands for converting the clipboard from HTML to MD
-;; and back.
-(if (eq system-type 'darwin)
-    (setq shell-command-history '("pandoc -f markdown_github -t html | pbcopy"
-                                  "pbpaste | pandoc -f html-native_divs-native_spans -t markdown_github")))
+;; On Mac and Windows WSL, add commands for converting the clipboard from HTML to MD and back.
+(cond ((eq system-type 'darwin)
+       (setq shell-command-history '("pandoc -f markdown_github -t html | pbcopy"
+                                     "pbpaste | pandoc -f html-native_divs-native_spans -t markdown_github")))
+      ((is-windows-wsl)
+       (setq shell-command-history '("pandoc -f markdown_github -t html | clip.exe"
+                                     "powershell.exe Get-Clipboard | pandoc -f html-native_divs-native_spans -t markdown_github"))))
 
 (add-to-list 'auto-mode-alist '("\\.ino$" . c-mode))
