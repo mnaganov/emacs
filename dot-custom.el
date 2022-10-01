f8457c2b624404b3736371d48d353a31db55ac51
Mikhail Naganov <mnaganov@google.com>
2022-10-01 16:44:02 +0000

    Replace most of grays with bright background colors

--- dot-custom.el
+++ dot-custom.el
@@ -17,6 +17,27 @@
  '(js2-indent-on-enter-key nil)
  '(js2-mirror-mode nil)
  '(ns-command-modifier 'meta))
+;; The following 16-color palette is recommended.
+;; It's based on Colorized Light, with most of shades of gray replaced
+;; with bright colors to use as backgrounds.
+;; [
+;;  "#073642",
+;;  "#b22222",
+;;  "#228b22",
+;;  "#b58900",
+;;  "#268bd2",
+;;  "#8b2252",
+;;  "#0087af",
+;;  "#eee8d5",
+;;  "#002b36",
+;;  "#cb4b16",
+;;  "#e3ffb3",
+;;  "#ffffaf",
+;;  "#b3e5ff",
+;;  "#3a5fcd",
+;;  "#93a1a1",
+;;  "#fdf6e3"
+;; ]
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -24,9 +45,11 @@
  ;; If there is more than one, they won't work right.
  '(diff-added ((t (:inherit diff-changed :foreground "green"))))
  '(diff-context ((t (:inherit shadow))))
- '(diff-file-header ((t (:foreground "brightgreen"))))
+ '(diff-file-header ((t (:foreground "brightblack"))))
  '(diff-header ((t (:background "brightcyan"))))
- '(diff-removed ((t (:inherit diff-changed :foreground "magenta"))))
+ '(diff-refine-added ((t (:inherit diff-added :background "brightgreen"))))
+ '(diff-refine-removed ((t (:inherit diff-removed :background "brightblue"))))
+ '(diff-removed ((t (:inherit diff-changed :foreground "cyan"))))
  '(error ((t (:foreground "red" :weight bold))))
  '(eww-form-text ((t (:foreground "brightblack" :background "brightcyan"))))
  '(font-lock-builtin-face ((t (:foreground "black"))))
@@ -37,12 +60,13 @@
  '(font-lock-preprocessor-face ((t (:foreground "brightblack"))))
  '(font-lock-string-face ((t (:foreground "magenta"))))
  '(font-lock-type-face ((t (:foreground "green"))))
- '(help-key-binding ((t (:background "brightcyan"))))
- '(hi-yellow ((t (:background "white"))))
+ '(help-key-binding ((t (:bold t))))
+ '(hi-yellow ((t (:background "brightyellow"))))
  '(highlight ((t (:background "white"))))
- '(lazy-highlight ((t (:inverse-video t))))
+ '(isearch ((t (:background "brightgreen"))))
+ '(lazy-highlight ((t (:background "brightyellow"))))
  '(magit-header-line ((t (:inherit header-line :bold t))))
- '(region ((t (:background "black" :distant-foreground "white"))))
+ '(region ((t (:background "brightblue"))))
  '(show-paren-match ((t (:bold t))))
  '(show-paren-mismatch ((t (:background "red"))))
  '(warning ((t (:foreground "brightred" :bold t))))
