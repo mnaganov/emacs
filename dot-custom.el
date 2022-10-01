5f1d6f8aaf9ffaf9e8da10ea8c946e46d94332a7
Mikhail Naganov <mikhail.naganov@gmail.com>
2022-09-30 20:56:16 -0700

    Better compatibility with 16 color mode

--- dot-custom.el
+++ dot-custom.el
@@ -1,34 +1,45 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ag-reuse-buffers t)
+ '(company-idle-delay 3)
+ '(compilation-scroll-output t)
+ '(direx:closed-icon "→")
+ '(direx:leaf-icon " ")
+ '(direx:open-icon "↓")
+ '(eldoc-echo-area-prefer-doc-buffer t)
+ '(eldoc-echo-area-use-multiline-p nil)
  '(js2-auto-indent-p nil)
  '(js2-basic-offset 2)
  '(js2-cleanup-whitespace nil)
  '(js2-indent-on-enter-key nil)
  '(js2-mirror-mode nil)
- '(ns-command-modifier (quote meta))
- '(direx:leaf-icon " ")
- '(direx:open-icon "\u2193")
- '(direx:closed-icon "\u2192")
- '(ag-reuse-buffers t)
- '(eldoc-echo-area-prefer-doc-buffer t)
- '(eldoc-echo-area-use-multiline-p nil)
- '(company-idle-delay 3)
- '(compilation-scroll-output t))
+ '(ns-command-modifier (quote meta)))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(diff-added ((t (:inherit diff-changed :foreground "green"))))
- '(diff-removed ((t (:inherit diff-changed :foreground "magenta"))))
  '(diff-context ((t (:inherit shadow))))
+ '(diff-removed ((t (:inherit diff-changed :foreground "magenta"))))
+ '(error ((t (:foreground "red" :weight bold))))
  '(eww-form-text ((t (:foreground "brightblack" :background "brightcyan"))))
- '(hi-yellow ((t (:background "color-229"))))
- '(magit-header-line ((t (:inherit header-line :bold t))))
- '(which-func ((t (:inherit header-line))))
+ '(font-lock-builtin-face ((t (:foreground "black"))))
+ '(font-lock-comment-face ((t (:foreground "red"))))
+ '(font-lock-constant-face ((t (:foreground "cyan"))))
+ '(font-lock-function-name-face ((t (:foreground "brightblack" :weight normal))))
+ '(font-lock-keyword-face ((t (:foreground "brightmagenta" :weight normal))))
+ '(font-lock-preprocessor-face ((t (:foreground "brightblack"))))
+ '(font-lock-string-face ((t (:foreground "magenta"))))
+ '(font-lock-type-face ((t (:foreground "green"))))
+ '(hi-yellow ((t (:background "white"))))
  '(highlight ((t (:background "white"))))
+ '(magit-header-line ((t (:inherit header-line :bold t))))
+ '(region ((t (:background "brightcyan" :distant-foreground "black"))))
  '(show-paren-match ((t (:bold t))))
- '(show-paren-mismatch ((t (:background "red")))))
+ '(show-paren-mismatch ((t (:background "red"))))
+ '(warning ((t (:foreground "brightred" :bold t))))
+ '(which-func ((t (:inherit header-line)))))
