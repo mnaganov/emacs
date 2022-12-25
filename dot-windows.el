322afaf8b9ecf9da5f84c403b783764601b81463
Mikhail Naganov <mikhail.naganov@gmail.com>
2022-12-24 19:41:17 -0800

    Remove cygwin helpers

--- dot-windows.el
+++ dot-windows.el
@@ -1,7 +1,5 @@
 ;; Windows-specific part
 
-;;(require 'setup-cygwin)
-
 (let ((grep-execs-path "c:/Program Files/Git/usr/bin/"))
   (setq grep-find-template
         (concat "\"" grep-execs-path "find.exe\" <D> <X> -type f <F> -exec \"" grep-execs-path "grep.exe\" <C> -nH --null <R> \"{}\" \";\"")))
