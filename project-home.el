9813170cec1ba25529919e9fa8607fb200c948ea
Mikhail Naganov <mikhail.naganov@gmail.com>
2024-07-06 16:21:35 -0700

    Updated Pandoc rendering command

--- project-home.el
+++ project-home.el
@@ -10,7 +10,7 @@
 ;; On Mac and Windows WSL, add commands for converting the clipboard from HTML to MD and back.
 (cond ((eq system-type 'darwin)
        (setq shell-command-history '("pandoc -f markdown_github -t html | pbcopy"
-                                     "pbpaste | pandoc -f html-native_divs-native_spans -t markdown_github")))
+                                     "pbpaste | pandoc -f html-native_divs-native_spans -t markdown | sed -e '1h;2,$H;$!d;g' -e 's/{[^{}]*}//g'")))
       ((is-windows-wsl)
        (setq shell-command-history '("pandoc -f markdown_github -t html | clip.exe"
                                      "powershell.exe Get-Clipboard | pandoc -f html-native_divs-native_spans -t markdown_github"))))
