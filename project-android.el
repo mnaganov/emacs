f406d266c9423c24124818c9de3a317c8420312f
Mikhail Naganov <mnaganov@google.com>
2025-09-22 08:59:32 -0700

    Updated project-android

--- project-android.el
+++ project-android.el
@@ -4,7 +4,7 @@
       '(". build/envsetup.sh && lunch aosp_cf_x86_64_only_phone-trunk_staging-userdebug && . build/make/rbesetup.sh && SOONG_GEN_COMPDB=1 SOONG_LINK_COMPDB_TO=$ANDROID_BUILD_TOP m"
         ". build/envsetup.sh && lunch aosp_cf_x86_64_only_phone-trunk_staging-userdebug && . build/make/rbesetup.sh && SOONG_GEN_COMPDB=1 SOONG_LINK_COMPDB_TO=$ANDROID_BUILD_TOP ROLLING_TF_SUBPROCESS_OUTPUT=0 atest VtsHalAudioCoreTargetTest"
         ". build/envsetup.sh && lunch tokay-trunk_staging-userdebug && . build/make/rbesetup.sh && SOONG_GEN_COMPDB=1 SOONG_LINK_COMPDB_TO=$ANDROID_BUILD_TOP m"
-        ". build/envsetup.sh && lunch tokay-trunk_staging-userdebug && . build/make/rbesetup.sh && SOONG_GEN_COMPDB=1 SOONG_LINK_COMPDB_TO=$ANDROID_BUILD_TOP ANDROID_SERIAL=3B141FDAQ000DJ ROLLING_TF_SUBPROCESS_OUTPUT=0 atest CtsMediaTestCases:AudioTrackTest"
+        ". build/envsetup.sh && lunch tokay-trunk_staging-userdebug && . build/make/rbesetup.sh && SOONG_GEN_COMPDB=1 SOONG_LINK_COMPDB_TO=$ANDROID_BUILD_TOP ANDROID_SERIAL=3B141FDAQ000DJ ROLLING_TF_SUBPROCESS_OUTPUT=0 atest CtsMediaAudioTestCases"
         "ANDROID_HOME=~/Android ANDROID_NDK_HOME=~/android-ndk-r22b ./gradlew assembleDebug"))
 (setq compile-command
    ". build/envsetup.sh && lunch tokay-trunk_staging-userdebug && . build/make/rbesetup.sh && SOONG_GEN_COMPDB=1 SOONG_LINK_COMPDB_TO=$ANDROID_BUILD_TOP m")
@@ -135,9 +135,8 @@
       (comint-send-input nil t))))
 (open-shell-buffer "=tokay=" (lambda() (insert-file-contents "~/screen/tokay.cfg" nil)))
 (open-shell-buffer "=husky=" (lambda() (insert-file-contents "~/screen/husky.cfg" nil)))
-(open-shell-buffer "=raven=" (lambda() (insert-file-contents "~/screen/raven.cfg" nil)))
+(open-shell-buffer "=blazer=" (lambda() (insert-file-contents "~/screen/blazer.cfg" nil)))
 (open-shell-buffer "=cuttlefish=" (lambda() (insert-file-contents "~/screen/cuttlefish.cfg" nil)))
-(open-shell-buffer "=aosp=" (lambda() (insert-file-contents "~/screen/aosp.cfg" nil)))
 ;; (open-shell-buffer "=toolbox=" (lambda() (insert (concat "cd ~/code/master && "
 ;;                                        ". build/envsetup.sh && lunch tokay-trunk_staging-userdebug && "
 ;;                                        "export ANDROID_SERIAL=3B141FDAQ000DJ "
