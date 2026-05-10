e59a21c0fdcb16eb536bbb916fda424535f5306f
Mikhail Naganov <mikhail.naganov@gmail.com>
2026-05-09 19:13:12 -0700

    Added command for typographic re-rendering via pandoc

--- project-home.el
+++ project-home.el
@@ -15,7 +15,8 @@
 ;; On Mac and Windows WSL, add commands for converting the clipboard from HTML to MD and back.
 (cond ((eq system-type 'darwin)
        (setq shell-command-history '("pandoc -f markdown_github -t html | pbcopy"
-                                     "pbpaste | pandoc -f html-native_divs-native_spans -t markdown | sed -e '1h;2,$H;$!d;g' -e 's/{[^{}]*}//g'")))
+                                     "pbpaste | pandoc -f html-native_divs-native_spans -t markdown | sed -e '1h;2,$H;$!d;g' -e 's/{[^{}]*}//g'"
+                                     "pandoc -f markdown+smart -t markdown-smart")))
       ((is-windows-wsl)
        (setq shell-command-history '("pandoc -f markdown_github -t html | clip.exe"
                                      "powershell.exe Get-Clipboard | pandoc -f html-native_divs-native_spans -t markdown_github"))))
